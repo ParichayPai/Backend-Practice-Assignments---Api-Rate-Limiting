afdf508fc599781e058940afaf1ac512d1cbaafb
ParichayPai <parichaypai393@gmail.com>
2021-01-23 17:25:20 +0530

    udpates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
+const posts = require('./initialData');
+const rateLimit = require("express-rate-limit");
+
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -13,6 +16,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
+const limiter = rateLimit({
+    windowMs: 30 * 1000, 
+    max: 5,
+    message: "Exceed Number of API Calls"
+});
+
+app.use(limiter);
+
+app.get('/api/posts', (req, res) => {
+    if(req.query.max){
+        let max = req.query.max;
+        if(max <= 20){
+            res.status(200).send(posts.splice(0, max));
+            return;
+        }
+    }
+    res.status(200).send(posts.splice(0, 10));
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
